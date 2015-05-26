fb124ae6ede7a434ce417244cb154bb65fe62d0d
Matthew Mitchell <mmitchell@Matts-Macbook.local>
2015-05-25 17:16:06 -0700

    test

--- web.rb
+++ web.rb
@@ -1,12 +1,4 @@
-require 'rubygems'
-require 'sinatra'
-require 'boxr'
-#require 'twilio-ruby'
-require 'awesome_print'
-require 'ap'
-require 'rufus-scheduler'
-#require 'dotenv'
-#Dotenv.load(".env")
+Dotenv.load(".env")
 
 
 MAX_REFRESH_TIME = 1800
